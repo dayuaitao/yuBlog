7e584a38c8a774659ef6feb2d04d27df8428a48b
yuaitao <3117277090@qq.com>
2019-03-15 13:54:20 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,19 @@
+var config = {
+
+    // 静态网站部署的目录
+    base: '/yuBlog/',
+
+    // 网站标题
+    title: '鱼博客',
+
+    // <meta name="description" content="...">
+    description: '大鱼向东，遇门而不入',
+
+    markdown: {
+
+        // 显示代码行号
+        lineNumbers: true
+    }
+}
+
+module.exports = config
---
+++ docs/README.md
@@ -0,0 +1,5 @@
+---
+home: true
+heroImage: /BigFish.jpeg
+footer: Fish in the sea | Copyright © 2018 BigFish
+---
